a4c8b4df13aa4929aefcc74d430f1eaf9b609a10
Ayeasha03 <oyinaishat3@gmail.com>
2025-04-27 20:44:28 +0100

    Fix deployment issues

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
+import { useDispatch } from 'react-redux';
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
-    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [showPlants, setShowPlants] = useState(false); 
     const [addedToCart, setAddedToCart] = useState({});
     const handleAddToCart = (product) => {
         dispatch(addItem(product));
@@ -14,6 +16,7 @@ function ProductList({ onHomeClick }) {
          }));
       };
 
+      const dispatch = useDispatch();
 
     const plantsArray = [
         {
