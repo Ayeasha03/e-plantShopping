b401c7f2d140229106331c4105fbd7cfce2bbc87
Ayeasha03 <oyinaishat3@gmail.com>
2025-04-28 08:25:10 +0100

    Deploy production build to docs/

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: "/e-plantShopping/",
+  build: {
+    outDir: "docs",      // ← output to docs/
+  },
   plugins: [react()],
 })
