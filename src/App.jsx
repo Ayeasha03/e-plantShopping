0692fe30fd38260f22e547c42c3da01b9d6dcd4a
Ayeasha03 <oyinaishat3@gmail.com>
2025-04-28 07:40:01 +0100

    Fix Vite base path for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,50 @@
-
-import React, { useState } from 'react';
-import ProductList from './ProductList';
-import './App.css';
-import AboutUs from './AboutUs';
+import React from 'react'
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import ProductList from './ProductList'
+import AboutUs from './AboutUs'
+import './App.css'
 
 function App() {
-  
-  const [showProductList, setShowProductList] = useState(false);
-
-  const handleGetStartedClick = () => {
-    setShowProductList(true);
-  };
-
-  const handleHomeClick = () => {
-    setShowProductList(false);
-  };
-
   return (
-    <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
-        <div className="background-image"></div>
-        <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          </div>
-          </div>
+    <Router>
+      <div className="app-container">
+        <Routes>
+          {/* Landing Page Route */}
+          <Route
+            path="/"
+            element={
+              <div className="landing-page">
+                <div className="background-image"></div>
+                <div className="content">
+                  <div className="landing_content">
+                    <h1>Welcome To Paradise Nursery</h1>
+                    <div className="divider"></div>
+                    <p>Where Green Meets Serenity</p>
 
+                    {/* Link to Products Page */}
+                    <Link to="/products">
+                      <button className="get-started-button">
+                        Get Started
+                      </button>
+                    </Link>
+                  </div>
+                  <div className="aboutus_container">
+                    <AboutUs />
+                  </div>
+                </div>
+              </div>
+            }
+          />
+
+          {/* Product Listing Route */}
+          <Route
+            path="/products"
+            element={<ProductList />}
+          />
+        </Routes>
       </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-        <ProductList onHomeClick={handleHomeClick}/>
-      </div>
-    </div>
-  );
+    </Router>
+  )
 }
 
-export default App;
-
-
-
+export default App
